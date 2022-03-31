97b55164a820b6d16310077468ca3d64be591fba
Baptiste Prevot <baptiste.prevot@student-cs.fr>
2022-03-31 17:54:32 +0200

    Added life cycle breakdown

--- diet_utils.py
+++ diet_utils.py
@@ -7,7 +7,7 @@ And each line would be how much of the food chosen you eat per year (?)
 from collections import defaultdict
 import numpy as np
 
-def random_diet(food_codes, mean_weight=10, std_dev_weight=5, n_foods=100, seed=42):
+def random_diet(food_codes, mean_weight=10, std_dev_weight=5, n_foods=1000, seed=42):
     """Returns a random diet"""
     random = np.random.default_rng(seed)
     chosen_foods = random.choice(food_codes, n_foods, replace=False)
--- main.py
+++ main.py
@@ -5,79 +5,106 @@ import numpy as np
 import requests
 
 from diet_utils import random_diet
+from color_scale import FOOD_COLOR_SCALE
 
 st.title("How's your diet ?")
 
 @st.cache
 def load_agribalyse():
     synthese_url = "https://drive.google.com/uc?export=download&id=1FLyHBVgPsOeChEvDhd_KZCVr9wEFh5Az"
-    detail_url = "https://drive.google.com/uc?export=download&id=1YlFMYUdDXbivkoOnrRGsolQq84zp-Rk3"
-    agb_synthese =  pd.read_csv(synthese_url, delimiter=",", decimal=",")
+    agb_synthesis =  pd.read_csv(synthese_url, delimiter=",", decimal=",")
+    print("Synthesis loaded")
+
+    detail_url = "https://drive.google.com/uc?export=download&id=1dxL_5Mm67ncoC28FCJsvaOJgAjH5DMD7"
     agb_detail = requests.get(detail_url).json()
+    print("Detail loaded")
+
+    env_impact_cats = list(sorted(agb_detail[0]["environmental_impact"].keys()))
+    step_names = list(agb_detail[0]["environmental_impact"]["Single Score EF"]["steps"].keys())
     multi_index = pd.MultiIndex.from_product(
-        [
-            agb_detail[0]["impact_environnemental"].keys(), 
-            agb_detail[0]["impact_environnemental"]["Score unique EF"]["etapes"].keys()], 
+        [env_impact_cats, step_names], 
         names=['Impact type', 'Life cycle step'])
-    life_cycle_detail = pd.DataFrame(
-        data=[
-            sum((list(val["etapes"].values()) for val in alim["impact_environnemental"].values()), start=[])
-            for alim in agb_detail
-        ], 
-        columns=multi_index
+
+    impact_data=[
+        [food["environmental_impact"][cat]["steps"][step] for cat in env_impact_cats for step in step_names]
+        for food in agb_detail
+    ]
+    life_cycle_detail = pd.concat(
+        (
+            pd.DataFrame(
+                data=[int(food["ciqual_AGB"]) for food in agb_detail], columns=["Ciqual Code"]
+            ),
+            pd.DataFrame(data=impact_data, columns=multi_index),
+        ), 
+        axis=1
     )
-    return agb_synthese, life_cycle_detail
+    return agb_synthesis, life_cycle_detail, env_impact_cats, step_names
 
-
-agribalyse, life_cycle_detail = load_agribalyse()
-agribalyse = agribalyse.copy(deep=True)
-
-st.text(life_cycle_detail[:10])
+AGRIBALYSE_ORIG, LIFE_CYCLE_DETAIL_ORIG, ENV_IMPACT_CATS, STEP_NAMES = load_agribalyse()
+AGRIBALYSE = AGRIBALYSE_ORIG.copy(deep=True)
+LIFE_CYCLE_DETAIL = LIFE_CYCLE_DETAIL_ORIG.copy(deep=True)
 
 # Temporary
 DIETS = dict(zip(
     ["Veggie", "Vegan", "Flexie", "Carnist", "Pesci", "Custom"],
-    [random_diet(agribalyse["Ciqual Code"], seed=i) for i in range(6)]
+    [random_diet(AGRIBALYSE["Ciqual Code"], seed=i) for i in range(6)]
 ))
 
-# Chosing colors for each food group
-cat_20 = "1f77b4aec7e8ff7f0effbb782ca02c98df8ad62728ff98969467bdc5b0d58c564bc49c94e377c2f7b6d27f7f7fc7c7c7bcbd22dbdb8d17becf9edae5"
-cat_20 = [f"#{cat_20[6*i:6*(i+1)]}" for i in range(20)]
-domain = np.array([
-    'Drinks', 'Baby foods', 'Fruits, vegetables, legumes and oilseeds',
-    'Starters and compound dishes', 'Meat, eggs, fish', 'Cereal products', 
-    'Culinary aids and various ingredients', 'Milk and dairy products', 'Fatty products',
-    'Ice creams and sorbets', 'Sweet products'
-])
-color_range = np.array([
-    cat_20[1], cat_20[13], cat_20[4],
-    cat_20[14], cat_20[6], cat_20[16],
-    cat_20[15], cat_20[2], cat_20[3],
-    cat_20[17], cat_20[18],
-])
-sort_idx = np.argsort(domain)
-FOOD_COLOR_SCALE=alt.Scale(domain=domain[sort_idx], range=color_range[sort_idx])
+# =========================== Charts  ================================
 
-
-def update_weights(database, diet):
-    database['weight'] = database['Ciqual Code'].map(diet)
+def update_weights(diet):
+    AGRIBALYSE['weight'] = AGRIBALYSE['Ciqual Code'].map(diet)
+    LIFE_CYCLE_DETAIL['weight'] = LIFE_CYCLE_DETAIL['Ciqual Code'].map(diet)
 
 left, right = st.columns(2)
 
 with left:
+    # =========================== Diet choice ================================
     st.header("Chose your diet")
     st.radio(
         "Chose your diet", 
         options=DIETS.keys(), 
         key="diet_chosen",
-        on_change=lambda : update_weights(agribalyse, DIETS[st.session_state.diet_chosen])
+        on_change=lambda : update_weights(DIETS[st.session_state.diet_chosen])
     )
-    update_weights(agribalyse, DIETS[st.session_state.diet_chosen])
+    update_weights(DIETS[st.session_state.diet_chosen])
 
 with right:
+    # =========================== Composition ================================
     st.header("Diet Composition (by weight)")
-    composition = alt.Chart(agribalyse).mark_arc().encode(
+    composition = alt.Chart(AGRIBALYSE).mark_arc().encode(
         theta=alt.Theta(field="weight", type="quantitative", aggregate="sum"),
         color=alt.Color(field="Food Group", type="nominal", scale=FOOD_COLOR_SCALE),
     )
     st.altair_chart(composition)
+
+# =========================== Life cycle step impact ================================
+st.header("Environmental impact at each step of the life cycle")
+
+st.selectbox(
+    label="Impact indicator",
+    options=ENV_IMPACT_CATS,
+    key="env_impact_cat",
+    index=ENV_IMPACT_CATS.index("Single Score EF")
+)
+
+st.markdown("For more information about the meaning of each indicator, see the [Agribalyse documentation](https://doc.agribalyse.fr/documentation/methodologie-acv). We do not report them here because what matters most in this visualization is the comparison of the different food groups and life cycle steps inside the diet.")
+
+weighted_impacts = LIFE_CYCLE_DETAIL[
+    [(st.session_state.env_impact_cat, step) for step in STEP_NAMES]
+].mul(LIFE_CYCLE_DETAIL["weight"], axis=0)
+weighted_impacts.columns = STEP_NAMES
+weighted_impacts = pd.concat([LIFE_CYCLE_DETAIL[["Ciqual Code"]], weighted_impacts], axis=1)
+
+data = weighted_impacts.merge(AGRIBALYSE[["Ciqual Code", "Food Group"]], on="Ciqual Code")
+life_cycle_impact = alt.Chart(data).mark_bar().transform_fold(
+    STEP_NAMES, as_=["step", "value"]
+).encode(
+    x=alt.X("Food Group:N", sort="-y"),
+    y=alt.Y("sum(value):Q", title="Impact in the given category"),
+    color=alt.Color("step:N", title="Life cycle step"),
+).properties(
+    height=500,
+)
+
+st.altair_chart(life_cycle_impact, use_container_width=True)
